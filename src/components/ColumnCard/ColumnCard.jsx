456a9f2e2a8710385c8f624db2400b8f2d7905b8
byvova <133043645+byvova@users.noreply.github.com>
2024-06-07 22:47:30 +0300

    Created card component

---
+++ src/components/ColumnCard/ColumnCard.jsx
@@ -0,0 +1,56 @@
+import css from './ColumnCard.module.css'
+
+export const ColumnCard = () => {
+    return (
+        <ul className={css.cardsList}>
+                <li className={css.cardContainer}>
+                    <div className={css.contentContainer}>
+                        <h4 className={css.contentTitle}>The Watch Spot Design</h4>
+                        <p className={css.contentText}>Create a visually stunning
+                            and eye-catching watch dial
+                            design that embodies our brand's
+                            essence of sleek aesthetics and
+                            modern elegance. Your design should
+                            be unique, innovative, and reflective
+                            of the latest trends in watch design.</p>
+                    </div>
+                    <div className={css.infoContainer}>
+                        
+                        <div className={css.priority}>
+                            <p className={css.infoTitle}>Priority</p>
+                            <div className={css.container}>
+                            <svg width="12px" height="12px">
+                                <use></use>
+                            </svg>
+                            <p className={css.infoText}>Low</p>
+                            </div>
+                        </div>
+                        <div className={css.deadlineContainer}>
+                            <p className={css.infoTitle}>Deadline</p>
+                            <div>
+                                <p className={css.infoText}>12/05/2023</p>
+                            </div>
+                        </div>
+                        <div className={css.infoIcons}>
+                            <button className={css.headerSvgButton}>
+                                <svg width="16px" height="16px">
+                                    <use></use>
+                                </svg>
+                            </button>
+                            <button className={css.headerSvgButton}>
+                                <svg width="16px" height="16px">
+                                    <use></use>
+                                </svg>
+                            </button>
+                            <button className={css.headerSvgButton}>
+                                <svg width="16px" height="16px">
+                                    <use></use>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    
+                </li>
+            </ul>
+    )
+}
--- src/components/NewColumn/NewColumn.jsx
+++ src/components/NewColumn/NewColumn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import css from './NewColumn.module.css'
 import { EditColumnModal } from 'components/ModalWindow/EditColumnModal/EditColumnModal';
+import { ColumnCard } from 'components/ColumnCard/ColumnCard';
 
 export const NewColumn = ({ onClose }) => {
 
@@ -31,56 +32,8 @@ export const NewColumn = ({ onClose }) => {
                     </button>
                 </div>
             </div>
-            <ul className={css.cardsList}>
-                <li className={css.cardContainer}>
-                    <div className={css.contentContainer}>
-                        <h4 className={css.contentTitle}>The Watch Spot Design</h4>
-                        <p className={css.contentText}>Create a visually stunning
-                            and eye-catching watch dial
-                            design that embodies our brand's
-                            essence of sleek aesthetics and
-                            modern elegance. Your design should
-                            be unique, innovative, and reflective
-                            of the latest trends in watch design.</p>
-                    </div>
-                    <div className={css.infoContainer}>
-                        
-                        <div className={css.priority}>
-                            <p className={css.infoTitle}>Priority</p>
-                            <div className={css.container}>
-                            <svg width="12px" height="12px">
-                                <use></use>
-                            </svg>
-                            <p className={css.infoText}>Low</p>
-                            </div>
-                        </div>
-                        <div className={css.deadlineContainer}>
-                            <p className={css.infoTitle}>Deadline</p>
-                            <div>
-                                <p className={css.infoText}>12/05/2023</p>
-                            </div>
-                        </div>
-                        <div className={css.infoIcons}>
-                            <button className={css.headerSvgButton}>
-                                <svg width="16px" height="16px">
-                                    <use></use>
-                                </svg>
-                            </button>
-                            <button className={css.headerSvgButton}>
-                                <svg width="16px" height="16px">
-                                    <use></use>
-                                </svg>
-                            </button>
-                            <button className={css.headerSvgButton}>
-                                <svg width="16px" height="16px">
-                                    <use></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    
-                </li>
-            </ul>
+            
+            <ColumnCard/>
 
             {showEditModal && <EditColumnModal onClose={handleCloseEdit} />}
         </div>
