185449188e09b85653544674a35c8eb6d732ef07
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-06-08 09:39:47 +0300

    fix-deploy

--- src/components/ModalWindow/AddColumnModal/AddColumnModal.jsx
+++ src/components/ModalWindow/AddColumnModal/AddColumnModal.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react';
-import css from './AddColumnModal.module.css'
+//import { useState } from 'react';
+import css from './AddColumnModal.module.css';
 
 export const AddColumnModal = ({ onClose }) => {
+  //  const [inputValue, setInputValue] = useState("");
 
-  const [inputValue, setInputValue] = useState("");
-
-
-  const saveValue = (e) => {
+  const saveValue = e => {
     console.log(e);
-  }
+  };
 
   return (
     <div className={css.modalBackdrop}>
-      <div className={css.header} onClick={(e) => e.stopPropagation()}>
+      <div className={css.header} onClick={e => e.stopPropagation()}>
         <div className={css.container}>
           <h2>Add Column</h2>
           <div className={css.closeContainer}>
             <button className={css.onCloseButton} onClick={onClose}>
-              <svg width={9} height={9}><use href="../../../images/sprite.svg#icon-x-close"></use></svg>
+              <svg width={9} height={9}>
+                <use href="../../../images/sprite.svg#icon-x-close"></use>
+              </svg>
             </button>
           </div>
           <form className={css.form} onSubmit={saveValue()} action="">
-            <input className={css.formInput} type="text" placeholder='Title'/>
-            <button className={css.formButton} type='submit'>
+            <input className={css.formInput} type="text" placeholder="Title" />
+            <button className={css.formButton} type="submit">
               <svg width={28} height={28}>
                 <use href="../../../images/sprite.svg#icon-plus"></use>
               </svg>
               <p className={css.submitButtonText}>Add</p>
             </button>
           </form>
-          </div>
         </div>
       </div>
+    </div>
   );
 };
