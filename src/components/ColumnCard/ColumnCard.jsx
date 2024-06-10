0463e49280f43399c240b4105618bfce1cf29c2c
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-06-10 10:17:40 +0300

    Update ColumnCard.jsx

--- src/components/ColumnCard/ColumnCard.jsx
+++ src/components/ColumnCard/ColumnCard.jsx
@@ -4,44 +4,50 @@ import { ColumnCardItem } from 'components/ColumnCardItem/ColumnCardItem';
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
 import { ProgressModal } from 'components/ModalWindow/ProgressModal/ProgressModal';
-import { AddCardModal } from 'components/ModalWindow/AddCardModal/AddCardModal'; 
+import { AddCardModal } from 'components/ModalWindow/AddCardModal/AddCardModal';
 
 export const ColumnCard = () => {
-    const [showProgressModal, setShowProgressModal] = useState(false);
-    const [showAddCardModal, setShowAddCardModal] = useState(false);
+  const [showProgressModal, setShowProgressModal] = useState(false);
+  const [showAddCardModal, setShowAddCardModal] = useState(false);
 
-    const handleOpenProgress = () => {
-        setShowProgressModal(true);
-    };
+  const handleOpenProgress = () => {
+    setShowProgressModal(true);
+  };
 
-    const handleCloseProgress = () => {
-        setShowProgressModal(false);
-    };
+  const handleCloseProgress = () => {
+    setShowProgressModal(false);
+  };
 
-    const handleAddCard = () => {
-        setShowAddCardModal(true);
-    };
+  const handleAddCard = () => {
+    setShowAddCardModal(true);
+  };
 
-    const handleCloseAddCard = () => {
-        setShowAddCardModal(false);
-    };
+  const handleCloseAddCard = () => {
+    setShowAddCardModal(false);
+  };
 
-    const cards = [1, 2, 3, 4]; 
+  const cards = [1, 2, 3, 4];
 
-    return (
-        <div className={css.column}>
-            <SimpleBar style={{ maxHeight: 450}}>
-                <ul className={css.cardsList}>
-                    {cards.map((card, index) => (
-                        <ColumnCardItem key={index} showProgressModal={showProgressModal} handleOpenProgress={handleOpenProgress} />
-                    ))}
-                </ul>
-            </SimpleBar>
-            <button className={css.addButton} onClick={handleAddCard}>Add another card</button> 
-            {showProgressModal && <ProgressModal onClose={handleCloseProgress} />}
-        </div>
-//  {showAddCardModal && <AddCardModal onClose={handleCloseAddCard} />}
-       
-    );
+  return (
+    <>
+      <div className={css.column}>
+        <SimpleBar style={{ maxHeight: 450 }}>
+          <ul className={css.cardsList}>
+            {cards.map((card, index) => (
+              <ColumnCardItem
+                key={index}
+                showProgressModal={showProgressModal}
+                handleOpenProgress={handleOpenProgress}
+              />
+            ))}
+          </ul>
+        </SimpleBar>
+        <button className={css.addButton} onClick={handleAddCard}>
+          Add another card
+        </button>
+        {showProgressModal && <ProgressModal onClose={handleCloseProgress} />}
+      </div>
+      {showAddCardModal && <AddCardModal onClose={handleCloseAddCard} />}
+    </>
+  );
 };
-
