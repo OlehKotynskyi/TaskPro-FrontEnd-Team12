46349f743baf326b7b0e37b79ce5ffdabb1a122b
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-06-11 12:05:00 +0200

    filters modal

--- src/components/ModalWindow/Filters/Filters.jsx
+++ src/components/ModalWindow/Filters/Filters.jsx
@@ -1,44 +1,41 @@
-import css from './Filters.module.css'
+import React, { useState } from 'react';
+import { ModalContainer } from '../Shared/ModalContainer/ModalContainer';
+import styles from './Filters.module.css';
 
 export const Filters = ({ onClose }) => {
+  const [labelColor, setLabelColor] = useState('black');
 
+  const getLabel = (value) => {
+    if (value === 'black') {
+      return 'Without priority';
+    } else if (value === 'blue') {
+      return 'Low';
+    } else if (value === 'pink') {
+      return 'Medium';
+    } else if (value === 'green') {
+      return 'High';
+    }
+    return "Unexpected priority";
+  };
 
   return (
-    <div className={css.modalBackdrop}>
-      <div className={css.header} onClick={(e) => e.stopPropagation()}>
-        <div className={css.container}>
-          <h2 className={css.filterLine}>Filters</h2>
-          <div className={css.closeContainer}>
-            <button className={css.onCloseButton} onClick={onClose}>
-              <svg width={9} height={9}><use></use></svg>
-            </button>
-          </div>
-                  <form className={css.form} action="">
-                      <div>
-                          <label className={css.formTitle}>Label color</label>
-                          <label className={css.formTitle}>Show all</label>
-                      </div>
-                      <div className={css.formContainer}>
-                          <div className={css.formRadio}>
-                              <input className={css.formInput} type="radio" id="withoutPriority" name="priority" value="withoutPriority" />
-                              <label className={css.formText}>Without priority</label>
-                          </div>
-                          <div className={css.formRadio}>
-                              <input className={css.formInput} type="radio" id="low" name="priority" value="low" />
-                              <label className={css.formText}>Low</label>
-                          </div>
-                          <div className={css.formRadio}>
-                              <input className={css.formInput} type="radio" id="medium" name="priority" value="medium" />
-                              <label className={css.formText}>Medium</label>
-                          </div>
-                          <div className={css.formRadio}>
-                              <input className={css.formInput} type="radio" id="high" name="priority" value="high" />
-                              <label className={css.formText}>High</label>
-                          </div>
-                        </div>
-          </form>
-          </div>
-        </div>
+    <ModalContainer modalTitle="Filters" onClose={onClose}>
+      <h3 className={styles.labelTitle}>Label color</h3>
+      <div className={styles.radioGroup}>
+        {['blue', 'pink', 'green', 'black'].map((color) => (
+          <label className={styles.radioLabel} key={color}>
+            <input
+              type="radio"
+              value={color}
+              checked={labelColor === color}
+              onChange={() => setLabelColor(color)}
+              className={styles.radioInput}
+            />
+            <span className={`${styles.customRadio} ${styles[`${color}Label`]}`}></span>
+            <span className={styles.labelText}>{getLabel(color)}</span>
+          </label>
+        ))}
       </div>
+    </ModalContainer>
   );
 };
