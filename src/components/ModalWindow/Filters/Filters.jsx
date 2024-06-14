6bf77fc183188dd364927add01d7287979c6c9b1
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-06-14 19:27:47 +0200

    fix filter modal

--- src/components/ModalWindow/Filters/Filters.jsx
+++ src/components/ModalWindow/Filters/Filters.jsx
@@ -2,39 +2,48 @@ import React, { useState } from 'react';
 import { ModalContainer } from '../Shared/ModalContainer/ModalContainer';
 import styles from './Filters.module.css';
 
-export const Filters = ({ onClose }) => {
-  const [labelColor, setLabelColor] = useState('black');
+// showPriority - значення фільтру відображення карток певного пріорітету
 
+export const Filters = ({ showPriority = 'all', onClose }) => {
+  const [priority, setPriority] = useState(showPriority);
+
+  const handleShowAll = () => {
+    setPriority('all');
+    onClose();
+  }
+  
   const getLabel = (value) => {
-    if (value === 'black') {
-      return 'Without priority';
-    } else if (value === 'blue') {
-      return 'Low';
-    } else if (value === 'pink') {
-      return 'Medium';
-    } else if (value === 'green') {
-      return 'High';
-    }
-    return "Unexpected priority";
+    const labels = {
+      without: 'Without priority',
+      low: 'Low',
+      medium: 'Medium',
+      high: 'High',
+    };
+    return labels[value];
   };
 
   return (
-    <ModalContainer modalTitle="Filters" onClose={onClose}>
-      <h3 className={styles.labelTitle}>Label color</h3>
-      <div className={styles.radioGroup}>
-        {['blue', 'pink', 'green', 'black'].map((color) => (
-          <label className={styles.radioLabel} key={color}>
-            <input
-              type="radio"
-              value={color}
-              checked={labelColor === color}
-              onChange={() => setLabelColor(color)}
-              className={styles.radioInput}
-            />
-            <span className={`${styles.customRadio} ${styles[`${color}Label`]}`}></span>
-            <span className={styles.labelText}>{getLabel(color)}</span>
-          </label>
-        ))}
+    <ModalContainer width={300} modalTitle="Filters" onClose={onClose}>
+      <div className={styles.container}>
+        <div>
+          <h3 className={styles.labelTitle}>Label color</h3>
+          <div className={styles.radioGroup}>
+            {['without', 'low', 'medium', 'high'].map((color) => (
+              <label className={styles.radioLabel} key={color}>
+                <input
+                  type="radio"
+                  value={color}
+                  checked={priority === color}
+                  onChange={() => setPriority(color)}
+                  className={styles.radioInput}
+                />
+                <span className={`${styles.customRadio} ${styles[`${color}Label`]}`}></span>
+                <span className={styles.labelText}>{getLabel(color)}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+        <button className={styles.showAll} onClick={handleShowAll}>Show all</button>
       </div>
     </ModalContainer>
   );
