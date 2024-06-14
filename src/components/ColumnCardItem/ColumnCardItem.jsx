7ebe1077da487d713f50debea83ae5ee26624185
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-06-14 12:51:45 +0300

    fix

--- src/components/ColumnCardItem/ColumnCardItem.jsx
+++ src/components/ColumnCardItem/ColumnCardItem.jsx
@@ -11,58 +11,53 @@ export const ColumnCardItem = ({
   const handleDelete = () => handleDeleteCard(index);
 
   return (
-    <>
-      <li className={css.cardContainer}>
-        <div className={css.contentContainer}>
-          <h4 className={css.contentTitle}>The Watch Spot Design</h4>
-          <p className={css.contentText}>
-            Create a visually stunning and eye-catching watch dial design that
-            embodies our brand's essence of sleek aesthetics and modern
-            elegance. Your design should be unique, innovative, and reflective
-            of the latest trends in watch design.
-          </p>
+    <li className={css.cardContainer}>
+      <div className={css.contentContainer}>
+        <h4 className={css.contentTitle}>The Watch Spot Design</h4>
+        <p className={css.contentText}>
+          Create a visually stunning and eye-catching watch dial design that
+          embodies our brand's essence of sleek aesthetics and modern elegance.
+          Your design should be unique, innovative, and reflective of the latest
+          trends in watch design.
+        </p>
+      </div>
+      <div className={css.infoContainer}>
+        <div className={css.priority}>
+          <p className={css.infoTitle}>Priority</p>
+          <div className={css.container}>
+            <p className={css.infoColor}></p>
+            <p className={css.infoText}>Low</p>
+          </div>
         </div>
-        <div className={css.infoContainer}>
-          <div className={css.priority}>
-            <p className={css.infoTitle}>Priority</p>
-            <div className={css.container}>
-              <p className={css.infoColor}></p>
-              <p className={css.infoText}>Low</p>
-            </div>
+        <div className={css.deadlineContainer}>
+          <p className={css.infoTitle}>Deadline</p>
+          <div>
+            <p className={css.infoText}>12/05/2023</p>
           </div>
-          <div className={css.deadlineContainer}>
-            <p className={css.infoTitle}>Deadline</p>
-            <div>
-              <p className={css.infoText}>12/05/2023</p>
-            </div>
-          </div>
-          <div className={css.infoIcons}>
+        </div>
+        <div className={css.infoIcons}>
+          <svg className={css.icon} width="16px" height="16px">
+            <use href={`${sprite}#icon-bell`}></use>
+          </svg>
+        </div>
+        <div className={css.infoIcons}>
+          <button onClick={handleOpenProgress} className={css.headerSvgButton}>
             <svg className={css.icon} width="16px" height="16px">
-              <use href={`${sprite}#icon-bell`}></use>
+              <use href={`${sprite}#icon-arrow-circle-broken`}></use>
             </svg>
-          </div>
-          <div className={css.infoIcons}>
-            <button
-              onClick={handleOpenProgress}
-              className={css.headerSvgButton}
-            >
-              <svg className={css.icon} width="16px" height="16px">
-                <use href={`${sprite}#icon-arrow-circle-broken`}></use>
-              </svg>
-            </button>
-            <button className={css.headerSvgButton} onClick={handleOpenEdit}>
-              <svg className={css.icon} width="16px" height="16px">
-                <use href={`${sprite}#icon-pencil`}></use>
-              </svg>
-            </button>
-            <button className={css.headerSvgButton} onClick={handleDelete}>
-              <svg className={css.icon} width="16px" height="16px">
-                <use href={`${sprite}#icon-trash`}></use>
-              </svg>
-            </button>
-          </div>
+          </button>
+          <button className={css.headerSvgButton} onClick={handleOpenEdit}>
+            <svg className={css.icon} width="16px" height="16px">
+              <use href={`${sprite}#icon-pencil`}></use>
+            </svg>
+          </button>
+          <button className={css.headerSvgButton} onClick={handleDelete}>
+            <svg className={css.icon} width="16px" height="16px">
+              <use href={`${sprite}#icon-trash`}></use>
+            </svg>
+          </button>
         </div>
-      </li>
-    </>
+      </div>
+    </li>
   );
 };
